88bb1f50f0cfa782ef1a9696174c873c22942ccf
JulianMeneses1 <julian.meneses11@gmail.com>
2023-01-20 16:25:32 -0300

    contraer nav bar

--- Proyecto-Angular/src/app/components/header/header.component.ts
+++ Proyecto-Angular/src/app/components/header/header.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],  
 })
-export class HeaderComponent {
 
+export class HeaderComponent {  
+  @ViewChild('navBar') navBar!:ElementRef;   
+  faBars = faBars
+
+  constructor(private renderer: Renderer2) {
+
+  } 
+  
+  contraerNav(){
+    this.renderer.removeClass(this.navBar.nativeElement,"show")  
+  }  
 }
+
