4d49d38f2d33d93a3b6842bc79a6dba8404b7043
JulianMeneses1 <julian.meneses11@gmail.com>
2023-01-26 17:24:00 -0300

    alternar scrolling al desplegar el nav

--- Proyecto-Angular/src/app/components/header/header.component.ts
+++ Proyecto-Angular/src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, Renderer2, ViewChild } from '@angular/core';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { faBars, faX } from '@fortawesome/free-solid-svg-icons';
 import { ModoEdicionService } from 'src/app/services/modo-edicion.service';
 import { Subscription } from 'rxjs';
 
@@ -13,8 +13,10 @@ import { Subscription } from 'rxjs';
 export class HeaderComponent {  
   @ViewChild('navBar') navBar!:ElementRef;   
   faBars = faBars;
+  faX = faX  
   modoEdicion:boolean=false;
   suscripcion?:Subscription;
+  scrollVisible:boolean=true
   
 
   constructor(private renderer: Renderer2,
@@ -24,9 +26,20 @@ export class HeaderComponent {
       }   
   
   contraerNav(){
-    this.renderer.removeClass(this.navBar.nativeElement,"show")  
-    
-  }      
+    this.renderer.removeClass(this.navBar.nativeElement,"show");
+    this.scrollVisible=true;
+    this.renderer.removeClass(document.body, 'scroll-hidden')      
+  } 
+  
+  alternarScrolling(){
+    this.scrollVisible=!this.scrollVisible;
+    if (!this.scrollVisible) {
+      this.renderer.addClass(document.body, 'scroll-hidden')
+    } else{
+      this.renderer.removeClass(document.body, 'scroll-hidden')
+    }
+
+  }
   
   alternarEdicion(){
     this.servicioEdicion.alternarEdicion()
